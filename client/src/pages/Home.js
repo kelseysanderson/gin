1defaff6389b073056a2483bfc4bb920532efeb4
kelseysanderson <78172284+kelseysanderson@users.noreply.github.com>
2021-05-14 11:15:08 -0500

    initial file setup

---
+++ client/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+function Home() {
+
+    return (
+    
+    )
+  }
+
+
+export default Home;
---
+++ client/src/pages/Player.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+function Player() {
+
+    return (
+    
+    )
+  }
+
+
+export default Player;
