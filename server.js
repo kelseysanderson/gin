0a4734416d322aa90bca8c5ee76fcf0a19199fac
Eric Heltemes <e.heltemes83@gmail.com>
2021-05-20 13:39:57 -0500

    conosle.log(process.env.nodenv)

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const server = require('http').createServer(app)
 const io = require('socket.io')(server)
 const PORT = process.env.PORT || 3001;
 
+console.log(process.env.NODE_ENV )
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
